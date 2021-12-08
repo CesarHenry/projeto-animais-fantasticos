b0a5ba1aeb1738bd5cfaecaa003e2a983ef4959e
Cesar Henrique Oliveira <cesar_rasec89@hotmail.com>
2021-12-08 09:10:12 -0300

    Update scroll-animacao.js

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -5,14 +5,16 @@ export default function initAnimacaoScroll() {
   function animaScroll() {
     sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - windowMetade;
-      if(sectionTop < 0) {
+      const isSectionVisible = (sectionTop - windowMetade) < 0;
+      if(isSectionVisible) {
         section.classList.add('ativo');
+      } else {
+        section.classList.remove('ativo');
       }
-    })
+    });
   }
+  
   animaScroll();
 
   window.addEventListener('scroll', animaScroll);
 }
-
